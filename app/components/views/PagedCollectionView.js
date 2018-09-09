a695e17cc77c37d9c40e9ec48f1b756cc8bcc946
Gerard del Castillo <gerarddelcastillo@gmail.com>
2018-09-09 22:54:26 +0200

    pagination controls

--- app/components/views/PagedCollectionView.js
+++ app/components/views/PagedCollectionView.js
@@ -9,6 +9,7 @@ import AlbumView from './AlbumView'
 export default Marionette.CollectionView.extend({
 	tagName: 'div',
 	childViewContainer: 'ul',
+	//TODO: abstract this class
 	childView: AlbumView,
 	template: pagedViewTmpt,
 	elementsPerPage: 10,
@@ -16,18 +17,27 @@ export default Marionette.CollectionView.extend({
 	totalElements: 0,
 	totalPages: 0,
 	ui: {
-		pager: '.pager button',
+		pager: '.pager',
+		pageNumButton: '.pager div[data-page]',
+		pageNextButton: '.pager div[data-next-page]',
+		pagePrevButton: '.pager div[data-prev-page]',
 	},
 	events: {
-		'click @ui.pager': 'nextPage'
+		'click @ui.pageNumButton': 'handlePagerChange',
+		'click @ui.pageNextButton': 'nextPage',
+		'click @ui.pagePrevButton': 'prevPage'
 	},
 	initialize() {
-		this.collectionChange()
+		this.collectionChange();
 		//this.listenTo(this.collection, 'add remove reset update', this.collectionChange)
 	},
-	collectionChange() {
-		this.totalElements = this.collection.length
-		this.totalPages = this.totalElements / this.elementsPerPage
+	/* Class Events */
+	onRender() {
+		this.updatePager()
+	},
+	/* Event handlers */
+	handlePagerChange(event) {
+		this.changePage(event.target.dataset.page)
 	},
 	nextPage() {
 		let next = this.page + 1
@@ -35,9 +45,46 @@ export default Marionette.CollectionView.extend({
 			this.changePage(this.page + 1)
 		}
 	},
+	prevPage() {
+		let next = this.page - 1
+		if (next >= 0) {
+			this.changePage(this.page - 1)
+		}
+	},
 	changePage(nPage) {
-		this.page = nPage
-		this.render()
+		this.page = parseInt(nPage)
+		this.$el.fadeTo(200, 0, ()=>{
+			this.render()
+			this.$el.fadeTo(200, 1)
+		})
+	},
+	updatePager() {
+		let pagerElement = this.getUI('pager')
+		pagerElement.html('')
+		//TODO: we can probably use a template here
+		let pagerHTML = ''
+		pagerHTML += '<div '
+		if (this.npage === 0) {
+			pagerHTML += 'class="disabled"'
+		}
+		pagerHTML += ' data-prev-page="true">&lt;</div>'
+		for (let i = 0; i < this.totalPages; i++) {
+			pagerHTML += '<div '
+			if (this.npage === i) {
+				pagerHTML += 'class="active"'
+			}
+			pagerHTML += ' data-page="' + i + '">+</div>'
+		}
+		pagerHTML += '<div '
+		if (this.npage === this.totalPages) {
+			pagerHTML += 'class="disabled"'
+		}
+		pagerHTML += ' data-next-page="true">&gt;</div>'
+		pagerElement.html(pagerHTML)
+	},
+	collectionChange() {
+		this.totalElements = this.collection.length
+		this.totalPages = this.totalElements / this.elementsPerPage
 	},
 	viewFilter(view, index, collection) {
 		return index >= this.elementsPerPage * this.page &&
