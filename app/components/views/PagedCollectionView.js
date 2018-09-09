af8c17b645b440316443db2785d797aef075b69e
Gerard del Castillo <gerarddelcastillo@gmail.com>
2018-09-09 20:04:57 +0200

    next page functionality

--- app/components/views/PagedCollectionView.js
+++ app/components/views/PagedCollectionView.js
@@ -13,14 +13,32 @@ export default Marionette.CollectionView.extend({
 	template: pagedViewTmpt,
 	elementsPerPage: 10,
 	page: 0,
+	totalElements: 0,
+	totalPages: 0,
+	ui: {
+		pager: '.pager button',
+	},
+	events: {
+		'click @ui.pager': 'nextPage'
+	},
 	initialize() {
-		this.listenTo(this.collection, 'add remove reset update', this.collectionChange)
+		this.collectionChange()
+		//this.listenTo(this.collection, 'add remove reset update', this.collectionChange)
 	},
 	collectionChange() {
 		this.totalElements = this.collection.length
 		this.totalPages = this.totalElements / this.elementsPerPage
 	},
-
+	nextPage() {
+		let next = this.page + 1
+		if (next < this.totalPages) {
+			this.changePage(this.page + 1)
+		}
+	},
+	changePage(nPage) {
+		this.page = nPage
+		this.render()
+	},
 	viewFilter(view, index, collection) {
 		return index >= this.elementsPerPage * this.page &&
 				index < this.elementsPerPage * (this.page + 1)
